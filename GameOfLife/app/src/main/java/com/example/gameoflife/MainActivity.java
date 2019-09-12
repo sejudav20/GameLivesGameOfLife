7853ac4fa07827b39fa37b05a9398826a877aeb6
Davin <davsej100@gmail.com>
2019-09-12 13:57:01 -0700

    Added buttons

--- GameOfLife/app/src/main/java/com/example/gameoflife/MainActivity.java
+++ GameOfLife/app/src/main/java/com/example/gameoflife/MainActivity.java
@@ -5,41 +5,54 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     static RecyclerViewAdapter rva;
-    boolean[][] data = new boolean[100][100];
+    boolean[][] data = new boolean[20][30];
 
-    public static void generationChange(boolean[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int e = 0; e < arr[i].length; e++) {
-                int neighbors = numNeighbors(arr, i, e);
-                if (arr[i][e]) {
-                    if (neighbors < 2 || neighbors > 3) {
-                        arr[i][e] = false;
-
-                    }
+    public static class generationalChange extends AsyncTask<boolean[][], Void, Void> {
 
 
-                } else {
-                    if (neighbors == 3) {
-                        arr[i][e] = true;
+        @Override
+        protected Void doInBackground(boolean[][]... booleans) {
+            boolean[][] arr = booleans[0];
+            for (int i = 0; i < arr.length; i++) {
+                for (int e = 0; e < arr[i].length; e++) {
+                    int neighbors = numNeighbors(arr, i, e);
+                    if (arr[i][e]) {
+                        if (neighbors < 2 || neighbors > 3) {
+                            arr[i][e] = false;
+
+                        }
+
+
+                    } else {
+                        if (neighbors == 3) {
+                            arr[i][e] = true;
+
+                        }
 
                     }
 
                 }
 
+
             }
+            rva.notifyDataSetChanged();
 
 
+            return null;
         }
-        rva.notifyDataSetChanged();
-
     }
+
 
     public static int numNeighbors(boolean[][] arr, int row, int column) {
         int count = 0;
@@ -126,30 +139,70 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView rv = findViewById(R.id.recyclerView);
         rva = new RecyclerViewAdapter(data);
         rv.setAdapter(rva);
-        Button pause = findViewById(R.id.pause);
-        Button next = findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener() {
+        final ImageButton pause = findViewById(R.id.pause);
+        final ImageButton next = findViewById(R.id.next);
+        final ImageButton clear = findViewById(R.id.clear);
+        clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                generationChange(data);
-            }
-        });
-        pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (play) {
-                    play = false;
-                } else {
-                    play = true;
+                for (boolean[] b : data) {
+                    for (boolean bo : b) {
+                        bo = false;
+
+
+                    }
+
+
                 }
             }
         });
+        final ImageButton random = findViewById(R.id.randomized);
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (boolean[] b : data) {
+                    for (boolean bo : b) {
+                        Random rand = new Random();
+                        if (rand.nextInt(10) < 3) {
+                            bo = true;
+                        } else {
+                            bo = false;
+                        }
 
 
+                    }
 
 
+                }
+            }
+
+    });
+        next.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick (View view){
+        new generationalChange().execute(data);
     }
+    });
+        pause.setOnClickListener(new View.OnClickListener()
 
+    {
+        @Override
+        public void onClick (View view){
+        if (play) {
+            play = false;
+            pause.setImageDrawable(getDrawable(R.drawable.ic_play_arrow_black_24dp));
+
+        } else {
+            play = true;
+            pause.setImageDrawable(getDrawable(R.drawable.ic_pause_black_24dp));
+
+
+        }
+    }
+    });
+
+
+}
 
 
 }
